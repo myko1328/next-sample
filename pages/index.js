3c7c35455edd03f24c4b17c89897085d9b025e30
mykomiparanum <m.miparanum@gmail.com>
2021-12-22 12:49:23 +0100

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,41 @@
-import { server } from '../config';
-import Head from 'next/head';
-import ArticleList from '../components/ArticleList';
+import { server } from '../config'
+import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
 export default function Home({ articles }) {
-	console.log('asd', articles);
+  console.log('asd', articles)
 
-	return (
-		<div>
-			<Head>
-				<title>Webdev News</title>
-				<meta name='keywords' content='web development, programming' />
-			</Head>
-			<h1>Welcome to Next</h1>
+  return (
+    <div>
+      <Head>
+        <title>Webdev News</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      <h1>Welcome to Next</h1>
 
-			{/* <ArticleList articles={articles} /> */}
-		</div>
-	);
+      <ArticleList articles={articles} />
+    </div>
+  )
 }
 
-export const getStaticProps = async () => {
-	const res = await fetch('http://localhost:3000/api/articles');
-	const articles = await res.json();
-	return {
-		props: {
-			articles,
-		},
-	};
-};
-
 // export const getStaticProps = async () => {
-// 	const res = await fetch(
-// 		`https://jsonplaceholder.typicode.com/posts?_limit=6`
-// 	);
+//   const res = await fetch(`${server}/api/articles`)
+//   const articles = await console.log(res.json())
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }
 
-// 	const articles = await res.json();
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
 
-// 	return {
-// 		props: {
-// 			articles,
-// 		},
-// 	};
-// };
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
+}
